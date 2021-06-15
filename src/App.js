7e7197422c4aadc988197b738489370f72c13cbd
Ahmed Ali <ahmedali.96@hotmail.com>
2021-06-14 22:00:15 -0400

    base set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.scss";
+import { countryCodes, countryList } from "./countryData";
+const App = () => {
+  const getFlagEmoji = (countryCode) =>
+    countryCode
+      .toUpperCase()
+      .replace(/./g, (char) =>
+        String.fromCodePoint(127397 + char.charCodeAt())
+      );
 
-function App() {
+  const [gameData, setGameData] = useState({
+    gameScore: 0,
+    countryAsked: "",
+  });
+
+  const randomCountry = (obj) => {
+    const keys = Object.keys(obj);
+    return obj[keys[(keys.length * Math.random()) << 0]];
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Flag Match</h1>
+        <h3>{gameData.gameScore}/10</h3>
       </header>
+
+      <div className="gameArea">
+        <p className="countryAsked">{randomCountry(countryList)}</p>
+
+        <div className="flagCont">
+          <div className="flagItem">
+            <div className="flag">{getFlagEmoji("so")}</div>
+          </div>
+          <div className="flagItem">
+            <div className="flag">{getFlagEmoji("so")}</div>
+          </div>
+          <div className="flagItem">
+            <div className="flag">{getFlagEmoji("so")}</div>
+          </div>
+          <div className="flagItem">
+            <div className="flag">{getFlagEmoji("so")}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
