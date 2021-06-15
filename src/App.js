73a9ae711ed7e393b895ca80856cf8d9e92c88ca
Ahmed Ali <ahmedali.96@hotmail.com>
2021-06-15 00:30:31 -0400

    added basic logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.scss";
-import { countryCodes, countryList } from "./countryData";
+import countryList from "./countryData";
+
 const App = () => {
   const getFlagEmoji = (countryCode) =>
     countryCode
@@ -9,15 +10,50 @@ const App = () => {
         String.fromCodePoint(127397 + char.charCodeAt())
       );
 
+  let countryCodes = Object.keys(countryList);
+
+  let countryCode1 = countryCodes[Math.floor(Math.random() * 249) + 1];
+  let countryCode2 = countryCodes[Math.floor(Math.random() * 249) + 1];
+  let countryCode3 = countryCodes[Math.floor(Math.random() * 249) + 1];
+  let countryCode4 = countryCodes[Math.floor(Math.random() * 249) + 1];
+  let countryCode5 = countryCodes[Math.floor(Math.random() * 249) + 1];
+  let countryCode6 = countryCodes[Math.floor(Math.random() * 249) + 1];
+
   const [gameData, setGameData] = useState({
     gameScore: 0,
-    countryAsked: "",
+    flagData: [
+      {
+        countryCode: countryCode1,
+        flag: getFlagEmoji(countryCode1),
+        name: countryList[countryCode1],
+      },
+      {
+        countryCode: countryCode2,
+        flag: getFlagEmoji(countryCode2),
+        name: countryList[countryCode2],
+      },
+      {
+        countryCode: countryCode3,
+        flag: getFlagEmoji(countryCode3),
+        name: countryList[countryCode3],
+      },
+      {
+        countryCode: countryCode4,
+        flag: getFlagEmoji(countryCode4),
+        name: countryList[countryCode4],
+      },
+      {
+        countryCode: countryCode5,
+        flag: getFlagEmoji(countryCode5),
+        name: countryList[countryCode5],
+      },
+      {
+        countryCode: countryCode6,
+        flag: getFlagEmoji(countryCode6),
+        name: countryList[countryCode6],
+      },
+    ],
   });
-
-  const randomCountry = (obj) => {
-    const keys = Object.keys(obj);
-    return obj[keys[(keys.length * Math.random()) << 0]];
-  };
 
   return (
     <div className="App">
@@ -27,21 +63,21 @@ const App = () => {
       </header>
 
       <div className="gameArea">
-        <p className="countryAsked">{randomCountry(countryList)}</p>
+        <p className="countryAsked">
+          {
+            gameData.flagData[
+              Math.floor(Math.random() * gameData.flagData.length)
+            ].name
+          }
+        </p>
 
         <div className="flagCont">
-          <div className="flagItem">
-            <div className="flag">{getFlagEmoji("so")}</div>
-          </div>
-          <div className="flagItem">
-            <div className="flag">{getFlagEmoji("so")}</div>
-          </div>
-          <div className="flagItem">
-            <div className="flag">{getFlagEmoji("so")}</div>
-          </div>
-          <div className="flagItem">
-            <div className="flag">{getFlagEmoji("so")}</div>
-          </div>
+          {gameData.flagData.map((i, k) => (
+            <div className="flagItem" key={k}>
+              <div className="flag">{i.flag}</div>
+              <p>{i.name}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
